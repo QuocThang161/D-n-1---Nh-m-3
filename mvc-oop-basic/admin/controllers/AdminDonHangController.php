735f89a82e2e04d80a4ce87ac7b4269c285231a1
thanhbv <thanhbv343@gamil.com>
2026-03-16 15:51:19 +0700

    Chi tiết đơn hàng

--- mvc-oop-basic/admin/controllers/AdminDonHangController.php
+++ mvc-oop-basic/admin/controllers/AdminDonHangController.php
@@ -18,6 +18,23 @@ class AdminDonHangController
         require_once './views/donhang/listDonHang.php';
     }
 
+    public function detailDonHang()
+    {
+        $don_hang_id = $_GET['id_don_hang'];
+
+        // Lấy thông tin đơn hàng ở bảng don_hangs
+        $donHang = $this->modelDonHang->getDetailDonHang($don_hang_id);
+
+        //lấy danh sách sản phẩm đã đặt của đơn hàng ở bảng chi tiết đơn hàng
+        $sanPhamDonHang = $this->modelDonHang->getListDonHang($don_hang_id);
+
+        $listTrangThaiDonHang = $this->modelDonHang->getAlltrangThaiDonHang();
+
+        require_once './views/donhang/detailDonHang.php';
+    }
+
+
+    
     
 
     // public function formEditSanPham()

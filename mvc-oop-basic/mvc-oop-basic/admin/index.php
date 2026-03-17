9ead59e839c7bf7045c953f3b917cf8dadf9d34c
Nguyen Quoc Thang <setokaibakx@gmail.com>
2026-03-18 03:07:49 +0700

    Quan ly tai khoan khach hang

--- mvc-oop-basic/mvc-oop-basic/admin/index.php
+++ mvc-oop-basic/mvc-oop-basic/admin/index.php
@@ -8,14 +8,13 @@ require_once '../commons/function.php'; // Hàm hỗ trợ
 require_once './controllers/AdminDanhMucController.php';
 require_once './controllers/AdminSanPhamController.php';
 require_once './controllers/AdminBaoCaoThongKeController.php';
-
-
+require_once './controllers/AdminDonHangController.php';
+require_once './controllers/AdminTaiKhoanController.php';
 // Require toàn bộ file Models
 require_once './models/AdminDanhMuc.php';
 require_once './models/AdminSanPham.php';
 require_once './models/AdminTaiKhoan.php';
-
-
+require_once '/.models/AdminDonHang.php';
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 $act = $_GET['act'] ?? '/';
--- mvc-oop-basic/mvc-oop-basic/commons/env.php
+++ mvc-oop-basic/mvc-oop-basic/commons/env.php
@@ -3,7 +3,7 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL', 'http://localhost/mvc-oop-basic/mvc-oop-basic/mvc-oop-basic/');
+define('BASE_URL', 'http://localhost/mvc-oop-basic/mvc-oop-basic/');
 // đường dẫn vào admin
 define('BASE_URL_ADMIN', BASE_URL . 'admin/');
 
